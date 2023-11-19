bdd0616287721e8b14a96067f5ea64dba86886e4
ANKIT <closetooankit@gmail.com>
2023-11-19 17:33:08 +0530

    Final commit

--- function.c
+++ function.c
@@ -3,46 +3,111 @@
 #include "stdlib.h"
 struct Data
 {
-    int result;
-    int mat_result[][100];
-};
-struct Data data;
-sum(){
-
-}
-substraction(){
-
-}
-multiplication(){
-
-}
-division(){
-
-}
-sqroot(){
-
-}
-exponent(){
-
-}
-tigno(){
-
-}
-mat_add(){
-
-}
-mat_sub(){
-
-}
-mat_mul(){
-
-}
-mat_det(){
-
-}
-printHistory()
+    float result;
+} data;
+float x,y,result;
+void sum()
 {
-    char ch;
+    printf("Enter two numbers: ");
+    scanf("%f %f", &x, &y);
+    result = x + y;
+    printf("%.2f + %.2f = %.2f\n",x,y,result);
+    printf("\n");
+}
+void substraction()
+{
+    printf("Enter two numbers: ");
+    scanf("%f %f", &x, &y);
+    result = x - y;
+    printf("%.2f - %.2f = %.2f\n",x,y,result);
+    printf("\n");
+}
+void multiplication()
+{
+    printf("Enter two numbers: ");
+    scanf("%f %f", &x, &y);
+    result = x * y;
+    printf("%.2f x %.2f = %.2f\n",x,y,result);
+    printf("\n");
+}
+void division()
+{
+    printf("Enter the dividend and divisor: ");
+    scanf("%f %f", &x, &y);
+    if (y != 0)
+    {
+        result = x / y;
+        printf("%.2f / %.2f = %.2f\n",x,y,result);
+    }
+    else
+    {
+        printf("Division by zero is not allowed.\n");
+    }
+    printf("\n");
+}
+void sqroot()
+{
+    printf("Enter a number: ");
+    scanf("%f", &x);
+    if (x >= 0)
+    {
+        result = sqrt(x);
+        printf("square root of %.2f is %.2f\n",x,result);
+    }
+    else
+    {
+        printf("Invalid input for square root.\n");
+    }
+    printf("\n");
+}
+void exponent()
+{
+    printf("Enter base and exponent: ");
+    scanf("%f %f", &x, &y);
+    result = pow(x, y);
+    printf("%.2f ^ %.2f = %.2f\n",x,y,result);
+    printf("\n");
+}
+void trigno()
+{
+    printf("1. Sine\n2. Cosine\n3. Tangent\n4. Cotangent\n5. Secant\n6. Cosecant\nEnter your choice: ");
+    int trigChoice;
+    scanf("%d", &trigChoice);
+    printf("Enter an angle in degrees: ");
+    scanf("%f", &x);
+    switch (trigChoice)
+    {
+    case 1:
+        result = sin(x * M_PI / 180);
+        printf("sin(%.2f) = %.2f\n",x,result);
+        break;
+    case 2:
+        result = cos(x * M_PI / 180);
+        printf("cos(%.2f) = %.2f\n",x,result);
+        break;
+    case 3:
+        result = tan(x * M_PI / 180);
+        printf("tan(%.2f) = %.2f\n",x,result);
+        break;
+    case 4:
+        result = 1/(tan(x * M_PI / 180));
+        printf("cot(%.2f) = %.2f\n",x,result);
+        break;
+    case 5:
+        result = 1/(cos(x * M_PI / 180));
+        printf("sec(%.2f) = %.2f\n",x,result);
+        break;
+    case 6:
+        result = 1/sin((x * M_PI / 180));
+        printf("cosec(%.2f) = %.2f\n",x,result);
+        break;
+    default:
+        printf("Invalid choice for trigonometric function.\n");
+    printf("\n");
+    }
+}
+void printHistory()
+{
     FILE *fpr;
     fpr=fopen("hist.dat","rb");
     if ( fpr == NULL )
@@ -53,9 +118,10 @@ printHistory()
     while(fread(&data, sizeof(data),1,fpr)==1)
     {
         int i;
-        printf("%d. %d\n",i,data.result);
+        printf("%d. %.2f\n",i,data.result);
         i++;
     }
-    printf("\n") ;
     fclose(fpr) ;
+    printf("*** End Of History ***\n");
+    printf("\n");
 }
--- index.c
+++ index.c
@@ -1,17 +1,13 @@
-#include "stdio.h"
-#include "math.h"
 #include "function.c"
 int choice;
 int main()
 {
     do
     {
-        float x=0,y=0,result=0;
         printf("Scientific Calculator\n");
         printf("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n");
         printf("5. Square Root\n6. Exponentiation\n7. Trigonometric Functions\n");
-        printf("8. Matrix Addition\n9. Matrix Substraction\n10. Matrix Multiplication\n11. Determinant Of a Matrix\n");
-        printf("12. Show History\n13. Exit\n");
+        printf("8. Show History\n0. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
         FILE *fp;
@@ -24,119 +20,48 @@ int main()
         switch(choice)
         {
         case 1:
-            printf("Enter two numbers: ");
-            scanf("%f %f", &x, &y);
-            result = x + y;
+            sum();
             data.result=result;
             fwrite(&data, sizeof(data),1,fp);
             fclose(fp);
             break;
         case 2:
-            printf("Enter two numbers: ");
-            scanf("%f %f", &x, &y);
-            result = x - y;
+            substraction();
             data.result=result;
             fwrite(&data, sizeof(data),1,fp);
             fclose(fp);
             break;
         case 3:
-            printf("Enter two numbers: ");
-            scanf("%f %f", &x, &y);
-            result = x * y;
+            multiplication();
             data.result=result;
             fwrite(&data, sizeof(data),1,fp);
             fclose(fp);
             break;
         case 4:
-            printf("Enter two numbers: ");
-            scanf("%f %f", &x, &y);
-            if (y != 0)
-            {
-                result = x / y;
-                data.result=result;
-                fwrite(&data, sizeof(data),1,fp);
-                fclose(fp);
-            }
-            else
-            {
-                printf("Division by zero is not allowed.\n");
-                continue;
-            }
+            division();
+            data.result=result;
+            fwrite(&data, sizeof(data),1,fp);
+            fclose(fp);
             break;
         case 5:
-            printf("Enter a number: ");
-            scanf("%f", &x);
-            if (x >= 0)
-            {
-                result = sqrt(x);
-                data.result=result;
-                fwrite(&data, sizeof(data),1,fp);
-                fclose(fp);
-            }
-            else
-            {
-                printf("Invalid input for square root.\n");
-                continue;
-            }
+            sqroot();
+            data.result=result;
+            fwrite(&data, sizeof(data),1,fp);
+            fclose(fp);
             break;
         case 6:
-            printf("Enter base and exponent: ");
-            scanf("%f %f", &x, &y);
-            result = pow(x, y);
+            exponent();
             data.result=result;
             fwrite(&data, sizeof(data),1,fp);
             fclose(fp);
             break;
         case 7:
-            printf("1. Sine\n2. Cosine\n3. Tangent\n4. Cotangent\n5. Secant\n6. Cosecant\nEnter your choice: ");
-            int trigChoice;
-            scanf("%d", &trigChoice);
-            printf("Enter an angle in degrees: ");
-            scanf("%f", &x);
-            switch (trigChoice)
-            {
-            case 1:
-                result = sin(x * M_PI / 180);
-                data.result=result;
-                fwrite(&data, sizeof(data),1,fp);
-                fclose(fp);
-                break;
-            case 2:
-                result = cos(x * M_PI / 180);
-                data.result=result;
-                fwrite(&data, sizeof(data),1,fp);
-                fclose(fp);
-                break;
-            case 3:
-                result = tan(x * M_PI / 180);
-                data.result=result;
-                fwrite(&data, sizeof(data),1,fp);
-                fclose(fp);
-                break;
-            case 4:
-                result = sin(x * M_PI / 180);
-                data.result=result;
-                fwrite(&data, sizeof(data),1,fp);
-                fclose(fp);
-                break;
-            case 5:
-                result = cos(x * M_PI / 180);
-                data.result=result;
-                fwrite(&data, sizeof(data),1,fp);
-                fclose(fp);
-                break;
-            case 6:
-                result = tan(x * M_PI / 180);
-                data.result=result;
-                fwrite(&data, sizeof(data),1,fp);
-                fclose(fp);
-                break;
-            default:
-                printf("Invalid choice for trigonometric function.\n");
-                continue;
-            }
+            trigno();
+            data.result=result;
+            fwrite(&data, sizeof(data),1,fp);
+            fclose(fp);
             break;
-        case 12:
+        case 8:
             fclose(fp);
             printHistory();
             continue;
@@ -147,8 +72,6 @@ int main()
             printf("Invalid choice. Please enter a valid option.\n");
             continue;
         }
-
-
     }
     while(choice!=0);
 }
