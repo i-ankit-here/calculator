a139cafcaa314b0802df7cce2536dd70b59cab34
ANKIT <closetooankit@gmail.com>
2023-12-14 10:47:48 +0530

    added expression in history and clear history option

--- function.c
+++ function.c
@@ -3,6 +3,7 @@
 #include "stdlib.h"
 struct Data
 {
+    char expression[50];
     float result;
 } data;
 float x,y,result;
@@ -80,31 +81,49 @@ void trigno()
     case 1:
         result = sin(x * M_PI / 180);
         printf("sin(%.2f) = %.2f\n",x,result);
+        sprintf(data.expression,"sin(%.2f) = ",x);
         break;
     case 2:
         result = cos(x * M_PI / 180);
+        sprintf(data.expression,"cos(%.2f) = ",x);
         printf("cos(%.2f) = %.2f\n",x,result);
         break;
     case 3:
         result = tan(x * M_PI / 180);
         printf("tan(%.2f) = %.2f\n",x,result);
+        sprintf(data.expression,"tan(%.2f) = ",x);
         break;
     case 4:
         result = 1/(tan(x * M_PI / 180));
         printf("cot(%.2f) = %.2f\n",x,result);
+        sprintf(data.expression,"cot(%.2f) = ",x);
         break;
     case 5:
         result = 1/(cos(x * M_PI / 180));
         printf("sec(%.2f) = %.2f\n",x,result);
+        sprintf(data.expression,"sec(%.2f) = ",x);
         break;
     case 6:
         result = 1/sin((x * M_PI / 180));
         printf("cosec(%.2f) = %.2f\n",x,result);
+        sprintf(data.expression,"cosec(%.2f) = ",x);
         break;
     default:
         printf("Invalid choice for trigonometric function.\n");
     printf("\n");
     }
+}
+void clearHistory()
+{
+    FILE *fpr;
+    fpr=fopen("hist.dat","wb");
+    if ( fpr == NULL )
+    {
+        puts("cannot open file") ;
+        exit(1) ;
+    }
+    printf("\n\n*****HISTORY IS CLEARED*****\n\n");
+    fclose(fpr);
 }
 void printHistory()
 {
@@ -115,13 +134,13 @@ void printHistory()
         puts("cannot open file") ;
         exit(1) ;
     }
+    int i=1;
     while(fread(&data, sizeof(data),1,fpr)==1)
     {
-        int i;
-        printf("%d. %.2f\n",i,data.result);
+        printf("%d. %s%.2f\n",i,data.expression,data.result);
         i++;
     }
     fclose(fpr) ;
-    printf("*** End Of History ***\n");
+    printf("\n\n*** End Of History ***\n");
     printf("\n");
 }
--- index.c
+++ index.c
@@ -4,12 +4,13 @@ int main()
 {
     do
     {
-        printf("Scientific Calculator\n");
-        printf("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n");
+        printf("\nScientific Calculator\n");
+        printf("\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n");
         printf("5. Square Root\n6. Exponentiation\n7. Trigonometric Functions\n");
-        printf("8. Show History\n0. Exit\n");
+        printf("8. Show History\n9. Clear History\n0. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
+        printf("\n\n");
         FILE *fp;
         fp=fopen("hist.dat","a");
         if ( fp == NULL )
@@ -21,36 +22,42 @@ int main()
         {
         case 1:
             sum();
+            sprintf(data.expression,"%.2f + %.2f = ",x,y);
             data.result=result;
             fwrite(&data, sizeof(data),1,fp);
             fclose(fp);
             break;
         case 2:
             substraction();
+            sprintf(data.expression,"%.2f - %.2f = ",x,y);
             data.result=result;
             fwrite(&data, sizeof(data),1,fp);
             fclose(fp);
             break;
         case 3:
             multiplication();
+            sprintf(data.expression,"%.2f x %.2f = ",x,y);
             data.result=result;
             fwrite(&data, sizeof(data),1,fp);
             fclose(fp);
             break;
         case 4:
             division();
+            sprintf(data.expression,"%.2f / %.2f = ",x,y);
             data.result=result;
             fwrite(&data, sizeof(data),1,fp);
             fclose(fp);
             break;
         case 5:
             sqroot();
+            sprintf(data.expression,"sqrt(%.2f) = ",x);
             data.result=result;
             fwrite(&data, sizeof(data),1,fp);
             fclose(fp);
             break;
         case 6:
             exponent();
+            sprintf(data.expression,"%.2f ^ %.2f = ",x,y);
             data.result=result;
             fwrite(&data, sizeof(data),1,fp);
             fclose(fp);
@@ -64,6 +71,10 @@ int main()
         case 8:
             fclose(fp);
             printHistory();
+            continue;
+        case 9:
+            fclose(fp);
+            clearHistory();
             continue;
         case 0:
             printf("Exiting the calculator.\n");
